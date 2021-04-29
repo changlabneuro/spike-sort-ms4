0f16f86e75fc82b6cbc187e6998a8f0cadabd8cc
nfagan <nick@fagan.org>
2021-04-29 11:43:02 -0400

    no positional arg for lambda

--- debug_run_file.py
+++ debug_run_file.py
@@ -9,14 +9,14 @@ PARALLEL = True
 
 if __name__ == '__main__':
     # _, src_filenames, _ = util.find_files(INPUT_ROOT, '.mat')
-    
+
     src_filenames = ['acc_1_04072016_kurosawacoppola_pre.mat', 
                      'acc_1_04122016_kurosawacoppola_pre.mat', 
                      'acc_1_04192016_kurosawacoppola_pre.mat',
                      'acc_1_04202016_kurosawacoppola_pre.mat']
 
     if PARALLEL:
-        fs = [lambda _: sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, f) for f in src_filenames]
+        fs = [lambda: sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, f) for f in src_filenames]
         tasks = multiprocess.make_tasks(fs)
         multiprocess.run_tasks(tasks)
 
