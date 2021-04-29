5a6670004097074b86eb07c8c50e6be44141d984
nfagan <nick@fagan.org>
2021-04-29 13:40:38 -0400

    remove parallel option for now

--- debug_run_files.py
+++ debug_run_files.py
@@ -5,7 +5,6 @@ import os
 
 INPUT_ROOT = '/media/chang/T41/data/mountainsort-plexon-pipeline/raw_data'
 OUTPUT_ROOT = '/media/chang/T41/data/mountainsort-plexon-pipeline/test_output'
-PARALLEL = False
 
 if __name__ == '__main__':
     # _, src_filenames, _ = util.find_files(INPUT_ROOT, '.mat')
@@ -15,10 +14,5 @@ if __name__ == '__main__':
                      'acc_1_04192016_kurosawacoppola_pre.mat',
                      'acc_1_04202016_kurosawacoppola_pre.mat']
 
-    fs = [lambda: sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, f) for f in src_filenames]
-
-    if PARALLEL:
-        multiprocess.run_tasks(multiprocess.make_tasks(fs))
-    else:
-        for task in fs:
-            task()
+    for f in src_filenames:
+        sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, f)
