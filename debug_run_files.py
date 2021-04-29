4c54bcc6df2445981ee749e42c863c9f650b40a7
nfagan <nick@fagan.org>
2021-04-29 14:09:17 -0400

    re-add parallel

--- debug_run_files.py
+++ debug_run_files.py
@@ -5,6 +5,10 @@ import os
 
 INPUT_ROOT = '/media/chang/T41/data/mountainsort-plexon-pipeline/raw_data'
 OUTPUT_ROOT = '/media/chang/T41/data/mountainsort-plexon-pipeline/test_output'
+PARALLEL = True
+
+def create_sorting_task(file):
+    return (lambda f: lambda: sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, f))(file)
 
 if __name__ == '__main__':
     # _, src_filenames, _ = util.find_files(INPUT_ROOT, '.mat')
@@ -14,5 +18,10 @@ if __name__ == '__main__':
                      'acc_1_04192016_kurosawacoppola_pre.mat',
                      'acc_1_04202016_kurosawacoppola_pre.mat']
 
-    for f in src_filenames:
-        sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, f)
+    fs = [create_sorting_task(f) for f in src_filenames]
+
+    if PARALLEL:
+        multiprocess.run_tasks(multiprocess.make_tasks(fs))
+    else:
+        for f in fs:
+            f()
