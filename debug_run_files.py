c830c7166da9e561ec787cbc927bba01b466970c
nfagan <nick@fagan.org>
2021-04-29 19:29:44 -0400

    specify max num processes

--- debug_run_files.py
+++ debug_run_files.py
@@ -1,24 +1,25 @@
 import sorting
 import util
 import multiprocess
+import numpy as np
 import os
 
 INPUT_ROOT = '/media/chang/T41/data/mountainsort-plexon-pipeline/raw_data'
 OUTPUT_ROOT = '/media/chang/T41/data/mountainsort-plexon-pipeline/test_output'
 PARALLEL = True
+NUM_PARALLEL_PROCESSES = 8
 
-def create_sorting_task(file):
-    return (lambda f: lambda: sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, f))(file)
+def sort_several(files):
+    for file in files:
+        sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, file)
+
+def create_sorting_task(files):
+    return (lambda f: lambda: sort_several(f))(files)
 
 if __name__ == '__main__':
     _, src_filenames, _ = util.find_files(INPUT_ROOT, '.mat')
-
-    # src_filenames = ['acc_1_04072016_kurosawacoppola_pre.mat',
-    #                  'acc_1_04122016_kurosawacoppola_pre.mat',
-    #                  'acc_1_04192016_kurosawacoppola_pre.mat',
-    #                  'acc_1_04202016_kurosawacoppola_pre.mat']
-
-    fs = [create_sorting_task(f) for f in src_filenames]
+    filename_sets = np.array_split(src_filenames, NUM_PARALLEL_PROCESSES)
+    fs = [create_sorting_task(f) for f in filename_sets]
 
     if PARALLEL:
         multiprocess.run_tasks(multiprocess.make_tasks(fs))
