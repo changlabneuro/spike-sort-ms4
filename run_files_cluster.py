e5785e2b0ac64e0bcc334ff71a98cbac961bcf97
Prabaha Gangopadhyay <prabaha.gangopadhyay@yale.edu>
2021-04-29 20:00:39 -0400

    add parameter for nunmber of parallel processes

--- run_files_cluster.py
+++ run_files_cluster.py
@@ -5,15 +5,19 @@ import os
 
 INPUT_ROOT = '/gpfs/milgram/project/chang/pg496/nn_all_raw_files'
 OUTPUT_ROOT = '/gpfs/milgram/project/chang/pg496/nn_all_raw_files_ms4_sorting'
-PARALLEL = True
+NUM_PARALLEL_PROCESSES = 28
 
-def create_sorting_task(file):
-    return (lambda f: lambda: sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, f))(file)
+def sort_several(files):
+    for file in files:
+        sorting.matlab_source_file_default_pipeline(INPUT_ROOT, OUTPUT_ROOT, file)
+
+def create_sorting_task(files):
+    return (lambda f: lambda: sort_several(f))(files)
 
 if __name__ == '__main__':
     _, src_filenames, _ = util.find_files(INPUT_ROOT, '.mat')
-
-    fs = [create_sorting_task(f) for f in src_filenames]
+    filename_sets = np.array_split(src_filenames, NUM_PARALLEL_PROCESSES)
+    fs = [create_sorting_task(f) for f in filename_sets]
 
     if PARALLEL:
         multiprocess.run_tasks(multiprocess.make_tasks(fs))
