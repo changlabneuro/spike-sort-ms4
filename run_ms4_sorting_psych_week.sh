ba0d9e20f8451c1a27a3422ff26cab885df4b3ca
Prabaha Gangopadhyay <pg496@milgram2.milgram.hpc.yale.internal>
2021-04-29 20:08:12 -0400

    add scripts to run in cluster

---
+++ run_ms4_sorting_psych_week.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --job-name=nn_all_raw_files_ms4_sorting
+#SBATCH --nodes=1 --ntasks=1 --cpus-per-task=28
+#SBATCH --mem-per-cpu=8G
+#SBATCH --mail-type=ALL
+#SBATCH --partition=psych_week --time=3-00:00:00
+
+
+module load miniconda
+conda activate ms4_sorting_env
+
+echo "Starting at $(date)"
+echo "Job submitted to the ${SLURM_JOB_PARTITION} partition, the default partition on ${SLURM_CLUSTER_NAME}"
+echo "Job name: ${SLURM_JOB_NAME}, Job ID: ${SLURM_JOB_ID}"
+echo "  I have ${SLURM_CPUS_ON_NODE} CPUs and ${mem_gbytes}GiB of RAM on compute node $(hostname)"
+
+python run_files_cluster.py
+
